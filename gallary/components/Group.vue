4915a3d91230487efdb8f4cf466f9d3609b854b1
sasayabaku <sasayabaku@yahoo.co.jp>
2022-05-08 21:00:13 +0900

    Dev : Edit World Coordinate

--- gallary/components/Group.vue
+++ gallary/components/Group.vue
@@ -8,6 +8,10 @@
                 <canvas id="group_canvas1"></canvas>
                 <h3>Group</h3>
             </b-col>
+            <b-col cols="12">
+                <canvas id="group_canvas2"></canvas>
+                <h3>World coordinate</h3>
+            </b-col>
         </b-row>
     </div>
 </template>
@@ -15,6 +19,7 @@
 export default {
     mounted() {
         init1();
+        init2();
     }
 };
 
@@ -69,4 +74,73 @@ function init1() {
         requestAnimationFrame(tick);
     }
 }
+
+function init2() {
+    const width = 300;
+    const height = 180;
+
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector("#group_canvas2")
+
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    // シーンを作成
+    const scene = new THREE.Scene();
+
+    // カメラ
+    const camera = new THREE.PerspectiveCamera(45, width / height);
+    camera.position.set(100, 150, 500);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    const plane2 = new THREE.GridHelper(600);
+    scene.add(plane2);
+    const plane = new THREE.AxesHelper(300);
+    scene.add(plane);
+
+    const group = new THREE.Group();
+    scene.add(group);
+
+    let targetMesh = new THREE.Mesh();
+
+    for (let i = 0; i < 10; i++) {
+        // 直方体作成
+        const material = i == 0 ? new THREE.MeshNormalMaterial() : new THREE.MeshBasicMaterial();
+        const geometry = new THREE.SphereGeometry(30, 30, 30);
+        const mesh = new THREE.Mesh(geometry, material);
+        const radian = (i / 10) * Math.PI * 2;
+        mesh.position.set(200 * Math.cos(radian), 0, 200 * Math.sin(radian));
+        group.add(mesh);
+
+        if(i === 0) {
+            targetMesh = mesh;
+        }
+    };
+
+    const geometry = new THREE.BufferGeometry().setFromPoints([
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(50, 50, 0),
+    ]);
+
+    const line = new THREE.Line(geometry, new THREE.LineBasicMaterial());
+    scene.add(line);
+
+    tick();
+
+    function tick() {
+
+        group.rotation.x += 0.02;
+        group.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+        requestAnimationFrame(tick);
+    }
+
+}
 </script>
+<style scoped>
+#container {
+    margin: 3rem;
+}
+</style>
