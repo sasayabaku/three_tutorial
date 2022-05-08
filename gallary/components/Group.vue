8adb33cbb2b2b03fc771315f8b57156d88f285be
sasayabaku <sasayabaku@yahoo.co.jp>
2022-05-08 23:00:23 +0900

    Dev: Screen Coordinete

--- gallary/components/Group.vue
+++ gallary/components/Group.vue
@@ -8,9 +8,19 @@
                 <canvas id="group_canvas1"></canvas>
                 <h3>Group</h3>
             </b-col>
-            <b-col cols="12">
+            <b-col cols="4">
                 <canvas id="group_canvas2"></canvas>
                 <h3>World coordinate</h3>
+            </b-col>
+            <b-col cols="4">
+                <canvas id="group_canvas3"></canvas>
+                <svg width="300" height="180" id="line_svg">
+                    <g stroke="white">
+                        <line id="svgLine" x1="0" y1="0" x2="0" y2="0" stroke-width="3" />
+                    </g>
+                </svg>
+                <div id="hud"></div>
+                <h3>Screen Corrdinate</h3>
             </b-col>
         </b-row>
     </div>
@@ -20,6 +30,7 @@ export default {
     mounted() {
         init1();
         init2();
+        init3();
     }
 };
 
@@ -138,9 +149,86 @@ function init2() {
     }
 
 }
+
+function init3() {
+    const width = 300;
+    const height = 180;
+    let rot = 0;
+
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector("#group_canvas3")
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(45, width / height);
+    camera.position.set(100, 150, 500);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    // 地面
+    const plane2 = new THREE.GridHelper(600);
+    scene.add(plane2);
+    const plane = new THREE.AxesHelper(300);
+    scene.add(plane);
+    
+    // 直方体
+    const material = new THREE.MeshNormalMaterial();
+    const geometry = new THREE.SphereGeometry(30, 30, 30);
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    tick();
+
+    function tick() {
+        rot += 0.5;
+        const radian = (rot * Math.PI) / 100;
+
+        mesh.position.x = 200 * Math.sin(radian);
+        mesh.position.y = 50;
+        mesh.position.z = 200 * Math.cos(radian);
+
+        renderer.render(scene, camera);
+
+        const worldPosition = mesh.getWorldPosition(new THREE.Vector3());
+
+        // スクリーン座標
+        const projection = worldPosition.project(camera);
+        const sx = (width / 2) * (+projection.x + 1.0);
+        const sy = (height / 2) * (-projection.y + 1.0);
+
+        const tf = document.getElementById('hud');
+        tf.innerHTML = `Screen Coordinate(${Math.round(sx)}, ${Math.round(sy)})`;
+        tf.style.transform = `translate(${sx}px, ${sy}px)`;
+
+        // SVGでら人を描画
+        const line = document.getElementById('svgLine');
+        line.setAttribute('x2', sx);
+        line.setAttribute('y2', sy);
+
+        requestAnimationFrame(tick);
+    }
+
+}
+
 </script>
 <style scoped>
 #container {
     margin: 3rem;
+}
+
+#hud { 
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: white;
+}
+
+#line_svg {
+    position: absolute;
+    top: 0;
+    left: 0;
 }
 </style>
